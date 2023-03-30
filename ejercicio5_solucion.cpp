e5daab577a68d3c0d1018833b71a08a444bef9c1
matiasgutierrez00 <126267809+matiasgutierrez00@users.noreply.github.com>
2023-03-30 17:42:40 -0400

    Create ejercicio5_solucion.cpp

---
+++ ejercicio5_solucion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int start, end;
+    cin >> start >> end;
+    
+    for(int i = start; i <= end; i++) {
+        cout << i << " ";
+    }
+    
+    cout << endl;
+    return 0;
+}
